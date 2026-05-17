7408fd886db9a75196c58a4812e6f253c62028cb
Niranjan18v <visuniranjan18@gmail.com>
2026-05-17 09:45:52 +0530

    Fix React Router basename for GitHub Pages subpath

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
